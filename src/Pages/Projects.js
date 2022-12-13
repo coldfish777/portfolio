cb4ea73c992de625a44aef41edcdc33a60228f19
SimingFeng <simingrox@gmail.com>
2022-12-13 17:11:41 -0500

    reworded project descriptions

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -14,28 +14,28 @@ function Projects() {
     >
       <ProjectCard
         title="Personas"
-        desc="I explored user personas and storyboarding to create a better printer experience for students."
+        desc="I explored user personas and storyboarding to create a better printer user experience for students."
         image={require("../Assets/1personas.jpg")}
         alt="Sketch of Printer"
         link="https://coldfish777.github.io/personas/"
       />
       <ProjectCard
         title="Redesign"
-        desc="I redesigned a website to responsively restructure its content based on the user's device, with a focus on accessibility."
+        desc="I redesigned and rebuilt a website to responsively restructure its content based on the user's device, with a focus on accessibility."
         image={require("../Assets/2redesign.png")}
         alt="Style Guide"
         link="https://coldfish777.github.io/redesign/"
       />
       <ProjectCard
         title="Iterative"
-        desc="I designed a desktop site for the startup Homeroom, with iterative improvements using feedback from both industry professionals and user testing."
+        desc="I designed a new desktop user interface for the startup Homeroom, with iterative improvements using feedback from both industry professionals and user testing."
         image={require("../Assets/3iterative.png")}
         alt="Homeroom App Home Page"
         link="https://coldfish777.github.io/iterative/"
       />
       <ProjectCard
         title="Development"
-        desc="I developed an online store using React, with custom implementations of filtering, sorting, and shopping cart."
+        desc="I built an online store using React, with custom implementations of filtering, sorting, and shopping cart."
         image={require("../Assets/4dev.png")}
         alt="Online Shop Home Page"
         link="https://coldfish777.github.io/development/"
