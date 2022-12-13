15c9f56ca5331be2cee74e27a13e29ac1caa0d35
SimingFeng <simingrox@gmail.com>
2022-12-13 16:54:34 -0500

    new home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,34 +1,34 @@
-import { Flex, Center, Stack, Text, Highlight } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import { TypeAnimation } from "react-type-animation";
 
 function Home() {
   return (
-    <Flex
-      width={"100vw"}
-      height={"100vh"}
-      alignContent={"center"}
-      justifyContent={"center"}
-    >
-      <Center m={5}>
-        <Stack spacing={10} align={"left"}>
-          <Text fontSize="9xl">
-            <Highlight
-              query="Hello"
-              styles={{ px: "2", py: "1", bg: "yellow" }}
-            >
-              Hello,
-            </Highlight>
-          </Text>
-          <Text fontSize="4xl">
-            <Highlight
-              query={["building experiences"]}
-              styles={{ px: "2", py: "1", bg: "yellow" }}
-            >
-              I am a designer focused on building experiences.
-            </Highlight>
-          </Text>
-        </Stack>
-      </Center>
-    </Flex>
+    <Box>
+      <Text fontSize="9xl" position="absolute" top="30vh" left="10vw" as="mark">
+        <TypeAnimation
+          // Same String at the start will only be typed once, initially
+          sequence={["Hello,", 1000]}
+          speed={1} // Custom Speed from 1-99 - Default Speed: 40
+          cursor={false} // Hide Cursor - Default: true
+        />
+      </Text>
+
+      <Text fontSize="5xl" position="absolute" top="55vh" left="10vw">
+        <TypeAnimation
+          // Same String at the start will only be typed once, initially
+          sequence={[
+            "I design unique experiences.",
+            2000,
+            "I build intuitive interfaces.",
+            2000,
+            "I engineer innovative software.",
+            2000,
+          ]}
+          speed={50} // Custom Speed from 1-99 - Default Speed: 40
+          repeat={Infinity} // Repeat this Animation Sequence infinitely
+        />
+      </Text>
+    </Box>
   );
 }
 
